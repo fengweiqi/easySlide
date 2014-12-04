1a12d97ecd7f141d6c373e45cce0b2e57e5c1672
fengweiqi <yakia@gm99.com>
2014-12-05 01:08:08 +0800

    完成对外接口

--- js/jquery.easySlide.js
+++ js/jquery.easySlide.js
@@ -6,79 +6,120 @@ email:yakia@gm99.com
 blog:fengweiqi.cn
 date:2014-09-08
 */ 
-(function($){
-	$.fn.easySlide=function(options){
-	   var defaults = { 
-			    slideTime:700,	//动画滑行速度，越大越慢
-			    autoPlay:true,	//true为自动播放，
-			    pauseTime:3000,	//动画暂停时间
-			    hoverPause:false, //是否鼠标悬停,默认为false
-			    index:0 //展示项目的索引
-		   };
-	   var opts = $.extend(defaults, options);
-	   var $this=$(this);
-	   var $globalWidth=$this.width();
-	   var $globalHeight=$this.height();
-	   var $slideItem=$this.find('.slideItem');
-	   var $itemLength=$slideItem.length;
-	   var $prev=$('#prev');
-	   var $next=$('#next');
-	   var slideAble=true;
-	   function init(){
-	   		$this.css('position', 'relative');
-	   		$slideItem.css({
-	   			width:$globalWidth,
-	   			height:$globalHeight,
-	   			position:'absolute',
-	   			top:0
-	   		});
-	   		$slideItem.eq(opts.index).css({
-	   			left:0
-	   		});
-	   		$slideItem.each(function(index,el){
-		   			$(this).css('left', -$globalWidth*(opts.index-index));
-	   			});
-	   	}
-	   	init();
-	    function prev(){
-	    	$slideItem.css('left',2*$globalWidth);//重置所有Item位置
-	    	$slideItem.eq(opts.index).css('left',0);//保持当前Item位置不变
-	    	var nowIndex=opts.index;//当前item索引
-	    	opts.index--;
-	    	opts.index=opts.index==-1?$itemLength-1:opts.index;//索引循环
-	    	var prevIndex=opts.index;
-	    	slideAble=false;
-	    	$slideItem.eq(prevIndex).css('left', -$globalWidth);
-	    	$slideItem.eq(prevIndex).animate({'left':0,}, opts.slideTime,function(){
-	    		slideAble=true;
-	    	});
-	    	$slideItem.eq(nowIndex).animate({'left':$globalWidth,}, opts.slideTime);
-	    }
-	    function next(){
-	    	$slideItem.css('left',2*$globalWidth);//重置所有Item位置
-	    	$slideItem.eq(opts.index).css('left',0);//保持当前Item位置不变
-	    	var nowIndex=opts.index;//当前item索引
-	    	opts.index++;
-	    	opts.index=opts.index==$itemLength?0:opts.index;//索引循环
-	    	var nextIndex=opts.index;
-	    	slideAble=false;
-	    	$slideItem.eq(nextIndex).css('left', $globalWidth);
-	    	$slideItem.eq(nextIndex).animate({'left':0,}, opts.slideTime,function(){
-	    		slideAble=true;
-	    	});
-	    	$slideItem.eq(nowIndex).animate({'left':-$globalWidth}, opts.slideTime);
-	    }
-	    //上一张
-	    $prev.click(function(event) {
-	    	if(slideAble)prev();
-	    	
-	    	
-	    });
-	    $next.click(function(event) {
-	    	if(slideAble)next();
-	    	
-	    });			
-		return this;
+;(function($, window, document,undefined) {
+	var Privateclass = function(el) {//私有类
+			this.opts=el.data('easySlide');
 	}
-	
-})($);
+	Privateclass.prototype={
+		prev:function(){
+			var opts=this.opts;
+    		if(opts.slideAble){
+    			opts.slideItem.css('left',2*opts.globalWidth);//重置所有Item位置
+		        opts.slideItem.eq(opts.index).css('left',0);//保持当前Item位置不变
+		        var nowIndex=opts.index;//当前item索引
+		        opts.index--;
+		        opts.index=opts.index==-1?opts.itemLength-1:opts.index;//索引循环
+		        var prevIndex=opts.index;
+		        slideAble=false;
+		        opts.slideItem.eq(prevIndex).css('left', -opts.globalWidth);
+		        opts.slideItem.eq(prevIndex).animate({'left':0,}, opts.slideTime,function(){
+		          slideAble=true;
+		        });
+		        opts.slideItem.eq(nowIndex).animate({'left':opts.globalWidth,}, opts.slideTime);
+
+    		};
+		}
+	}
+	var methods = {//对外接口
+		init: function(options) {
+			return this.each(function() {
+				var $this = $(this);
+				var opts = $this.data('easySlide');
+				if(typeof(opts) == 'undefined') {
+
+					var defaults = { 
+						    slideTime:700,	//动画滑行速度，越大越慢
+						    autoPlay:true,	//true为自动播放，
+						    pauseTime:3000,	//动画暂停时间
+						    hoverPause:false, //是否鼠标悬停,默认为false
+						    index:0 //展示项目的索引
+					   };
+
+					opts = $.extend({}, defaults, options);
+					$this.data('easySlide', opts);
+
+				} else {
+					opts = $.extend({}, opts, options);
+				}
+
+				// 代码在这里运行
+				var $globalWidth = $this.width();
+				var $globalHeight = $this.height();
+				var $slideItem = $this.find('.slideItem');
+				var $itemLength = $slideItem.length;
+				var $prev=$("#prev");
+				var slideAble = true;
+				var runSettings={//插件运行时的配置
+					globalWidth:$globalWidth,
+					globalHeight:$globalHeight,
+					slideItem:$slideItem,
+					itemLength:$itemLength,
+					slideAble:slideAble
+
+				}
+				opts = $.extend({}, opts, runSettings);
+				$this.data('easySlide', opts);
+				$this.css('position', 'relative');
+				$slideItem.css({
+					width: $globalWidth,
+					height: $globalHeight,
+					position: 'absolute',
+					top: 0
+				});
+				$slideItem.eq(opts.index).css({
+					left: 0
+				});
+				$slideItem.each(function(index, el) {
+					$(this).css('left', -$globalWidth * (opts.index - index));
+				});
+				
+				var privateclass=new Privateclass($this);
+				$prev.click(function(event) {
+					
+					return privateclass.prev();
+				});
+
+			});
+		},
+		destroy: function() {
+			return $(this).each(function() {
+				var $this = $(this);
+
+				$this.removeData('easySlide');
+			});
+		},
+		prev: function() {
+			var privateclass=new Privateclass(this);
+			return privateclass.prev();
+		   
+		}
+	};
+
+	$.fn.easySlide = function() {
+		var method = arguments[0];
+
+		if(methods[method]) {
+			method = methods[method];
+			arguments = Array.prototype.slice.call(arguments, 1);
+		} else if( typeof(method) == 'object' || !method ) {
+			method = methods.init;
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.easySlide' );
+			return this;
+		}
+
+		return method.apply(this, arguments);
+
+	}
+
+})(jQuery, window, document);
